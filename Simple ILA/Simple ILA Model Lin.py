8d74e562bc2db709cc4af0d4e93a4b17e4c71920
Kerk Phillips <kerk@pomosa.com>
2017-10-23 16:57:50 -0600

    Added 1-period-ahead forecast errors to Linearization code

--- Simple ILA/Simple ILA Model Lin.py
+++ Simple ILA/Simple ILA Model Lin.py
@@ -196,6 +196,18 @@ def PolSim(initial, nobs, ts, coeffs1, state1, params1, coeffs2, state2, \
     ihist = np.zeros(nobs)
     uhist = np.zeros(nobs)
     
+    # preallocate forecast histories
+    kfhist = np.zeros(nobs+1)
+    ellfhist = np.zeros(nobs)
+    zfhist = np.zeros(nobs)
+    Yfhist = np.zeros(nobs)
+    wfhist = np.zeros(nobs)
+    rfhist = np.zeros(nobs)
+    Tfhist = np.zeros(nobs)
+    cfhist = np.zeros(nobs)
+    ifhist = np.zeros(nobs)
+    ufhist = np.zeros(nobs)
+    
     # upack simulation parameters
     rho_z = params1[7] 
     sigma_z = params1[8]
@@ -226,6 +238,21 @@ def PolSim(initial, nobs, ts, coeffs1, state1, params1, coeffs2, state2, \
         Yhist[t], whist[t], rhist[t], Thist[t], chist[t], ihist[t], \
             uhist[t] = \
             Modeldefs(khist[t+1], khist[t], ellhist[t], zhist[t], params)
+        
+        # get 1-period ahead forecasts
+        zfhist[t+1] = rho_z*zhist[t]
+        kin = np.array([khist[t+1] - kbar])
+        zin = np.array([zfhist[t]])
+        kf, ellf = LinApp_Sim(kin, zin, PP, QQ, UU, RR, SS, VV)
+        # k and ell are deviations from SS values, so add these back.
+        # they are also 1D numpy arrays, so pull out the values rather than 
+        # use the arrays.
+        kfhist[t+2] = kf + kbar
+        ellfhist[t] = ellf + ellbar
+        Yfhist[t+1], wfhist[t+1], rfhist[t+1], Tfhist[t+1], cfhist[t+1], \
+            ifhist[t], ufhist[t] = Modeldefs(kfhist[t+2], khist[t+1], \
+            ellfhist[t+1], zfhist[t+1], params)
+        
     
     for t in range(ts-1, nobs):
         kin = np.array([khist[t] - kbar2])
@@ -236,9 +263,26 @@ def PolSim(initial, nobs, ts, coeffs1, state1, params1, coeffs2, state2, \
         Yhist[t], whist[t], rhist[t], Thist[t], chist[t], ihist[t], \
             uhist[t] = \
             Modeldefs(khist[t+1], khist[t], ellhist[t], zhist[t], params2)
+        
+        if t < nobs - 1:
+            # get 1-period ahead forecasts
+            zfhist[t+1] = rho_z*zhist[t]
+            kin = np.array([khist[t+1] - kbar])
+            zin = np.array([zfhist[t]])
+            kf, ellf = LinApp_Sim(kin, zin, PP2, QQ2, UU2, RR2, SS2, VV2)
+            # k and ell are deviations from SS values, so add these back.
+            # they are also 1D numpy arrays, so pull out the values rather than 
+            # use the arrays.
+            kfhist[t+2] = kf + kbar
+            ellfhist[t] = ellf + ellbar
+            Yfhist[t+1], wfhist[t+1], rfhist[t+1], Tfhist[t+1], cfhist[t+1], \
+                ifhist[t], ufhist[t] = Modeldefs(kfhist[t+2], khist[t+1], \
+                ellfhist[t+1], zfhist[t+1], params)
+    
     
     return khist, ellhist, zhist, Yhist, whist, rhist, Thist, chist, ihist, \
-        uhist
+        uhist, kfhist, ellfhist, zfhist, Yfhist, wfhist, rfhist, Tfhist, \
+        cfhist, ifhist, ufhist
 
 
 
@@ -260,7 +304,8 @@ coeffs2 = (PP2, QQ2, UU2, RR2, SS2, VV2)
 # get a time zero prediction
 params3 = np.array([alpha, beta, gamma, delta, chi, theta, tau, rho_z, 0.])
 
-kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, ipred, upred = \
+kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, ipred, upred,  \
+kf, ellf, zf, Yf, wf, rf, Tf, cf, invf, uf = \
     PolSim(initial, nobs, ts, coeffs1, XYbar, params3, coeffs2, XYbar2, \
            params3)
 
@@ -269,15 +314,70 @@ kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, ipred, upred = \
 nsim = 1000
 
 # run first simulation and store in Monte Carlo matrices
-kmc, ellmc, zmc, Ymc, wmc, rmc, Tmc, cmc, imc, umc \
+kmc, ellmc, zmc, Ymc, wmc, rmc, Tmc, cmc, imc, umc, \
+kfmc, ellfmc, zfmc, Yfmc, wfmc, rfmc, Tfmc, cfmc, ifmc, ufmc \
     = PolSim(initial, nobs, ts, coeffs1, XYbar, params, coeffs2, XYbar2, \
              params2)
 
+# replace forecast with abs value of forecast error
+for t in range(1, nobs):
+    kfmc[t] = np.abs(kfmc[t] - kmc[t])
+    ellfmc[t] = np.abs(ellfmc[t] - ellmc[t])
+    zfmc[t] = np.abs(zfmc[t] - zmc[t])
+    Yfmc[t] = np.abs(Yfmc[t] - Ymc[t])
+    wfmc[t] = np.abs(wfmc[t] - wmc[t])
+    rfmc[t] = np.abs(rfmc[t] - rmc[t])
+    Tfmc[t] = np.abs(Tfmc[t] - Tmc[t])
+    cfmc[t] = np.abs(cfmc[t] - cmc[t])
+    ifmc[t] = np.abs(ifmc[t] - imc[t])
+    ufmc[t] = np.abs(ufmc[t] - umc[t])
+
+# caclulate mean forecast errors
+foremeanmc = np.array([np.mean(kfmc[1:nobs]),
+                       np.mean(ellfmc[1:nobs]),
+                       np.mean(zfmc[1:nobs]), 
+                       np.mean(Yfmc[1:nobs]),
+                       np.mean(wfmc[1:nobs]), 
+                       np.mean(rfmc[1:nobs]),
+                       np.mean(Tfmc[1:nobs]), 
+                       np.mean(cfmc[1:nobs]),
+                       np.mean(ifmc[1:nobs]), 
+                       np.mean(ufmc[1:nobs])])                                 
+                                   
+                                   
 for i in range(1, nsim):
     # run remaining simulations
-    khist, ellhist, zhist, Yhist, whist, rhist, Thist, chist, ihist, uhist = \
+    khist, ellhist, zhist, Yhist, whist, rhist, Thist, chist, ihist, uhist, \
+    kfhist, ellfhist, zfhist, Yfhist, wfhist, rfhist, Tfhist, cfhist, ifhist, \
+    ufhist= \
         PolSim(initial, nobs, ts, coeffs1, XYbar, params, coeffs2, XYbar2, \
                params2)
+        
+    # replace forecast with abs value of forecast error
+    for t in range(1, nobs):
+        kfhist[t] = np.abs(kfhist[t] - khist[t])
+        ellfhist[t] = np.abs(ellfhist[t] - ellhist[t])
+        zfhist[t] = np.abs(zfhist[t] - zhist[t])
+        Yfhist[t] = np.abs(Yfhist[t] - Yhist[t])
+        wfhist[t] = np.abs(wfhist[t] - whist[t])
+        rfhist[t] = np.abs(rfhist[t] - rhist[t])
+        Tfhist[t] = np.abs(Tfhist[t] - Thist[t])
+        cfhist[t] = np.abs(cfhist[t] - chist[t])
+        ifhist[t] = np.abs(ifhist[t] - ihist[t])
+        ufhist[t] = np.abs(ufhist[t] - uhist[t])
+        
+    # caclulate mean forecast errors
+    foremean = np.array([np.mean(kfhist[1:nobs]),
+                         np.mean(ellfhist[1:nobs]),
+                         np.mean(zfhist[1:nobs]), 
+                         np.mean(Yfhist[1:nobs]),
+                         np.mean(wfhist[1:nobs]), 
+                         np.mean(rfhist[1:nobs]),
+                         np.mean(Tfhist[1:nobs]), 
+                         np.mean(cfhist[1:nobs]),
+                         np.mean(ifhist[1:nobs]), 
+                         np.mean(ufhist[1:nobs])])   
+        
     # stack results in Monte Carlo matrices
     kmc = np.vstack((kmc, khist))
     ellmc = np.vstack((ellmc, ellhist))
@@ -289,6 +389,7 @@ for i in range(1, nsim):
     cmc = np.vstack((cmc, chist))
     imc = np.vstack((imc, ihist))
     umc = np.vstack((umc, uhist))
+    foremeanmc = np.vstack((foremeanmc, foremean))
     
 # now sort the Monte Carlo matrices over the rows
 kmc = np.sort(kmc, axis = 0)
