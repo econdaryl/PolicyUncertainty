Corrected some minor stylistic things in VFI and GSSA code.  Added computation of actual SS to runmc.

Simple ILA/ILArunmc.py
@@ -57,11 +57,33 @@ def runmc(simargs, nsim, nobs, repincr):
     params4 = np.array([alpha, beta, gamma, delta, chi, theta, tau2, rho_z, \
                         0.])
     
+    # find actual steady state for baseline
     # get list of arguments for predictions simulation
+    predargs = (initial, nobs, nobs, generateLIN, args1, args2, params3, \
+                params3)
+    # simulate with zero shocks and see what k converges to in last period
+    kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, ipred, upred, \
+    kf, ellf, zf, Yf, wf, rf, Tf, cf, invf, uf, MsqEerr = polsim(predargs)
+    
+    # find actual (uncertainty) steady state values for baseline
+    kact = kpred[nobs-1]
+    ellact = ellpred[nobs-1]
+    Yact = Ypred[nobs-1]
+    wact = wpred[nobs-1]
+    ract = rpred[nobs-1]
+    Tact = Tpred[nobs-1]
+    cact = cpred[nobs-1]
+    iact = ipred[nobs-1]
+    uact = upred[nobs-1]
+    act = (kact, ellact, Yact, wact, ract, Tact, cact, iact, uact)
+    
+    initial = (kact, 0.)
+
+    # get NEW list of arguments for predictions simulation
     predargs = (initial, nobs, ts, generateLIN, args1, args2, params3, params4)
     
     # find predicted series
-    kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, ipred, upred,  \
+    kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, ipred, upred, \
     kf, ellf, zf, Yf, wf, rf, Tf, cf, invf, uf, MsqEerr = polsim(predargs)
     
     # preallocate mc matrices
@@ -148,4 +170,4 @@ def runmc(simargs, nsim, nobs, repincr):
         preddata = (kpred, ellpred, zpred, Ypred, wpred, rpred, Tpred, cpred, \
                     ipred, upred)
         
-    return mcdata, histdata, preddata
+    return mcdata, histdata, preddata, act

Simple ILA/gssa.py
@@ -239,23 +239,26 @@ def GSSA(params, kbar):
             # plot time series
         if count % 10 == 0:
             timeperiods = np.asarray(range(0,T))
+            plt.subplot(2,1,1)
             plt.plot(timeperiods, X1, label='X')
-            plt.axhline(y=kbar, color='r')
+            #plt.axhline(y=kbar, color='r')
+            plt.subplot(2,1,2)
             plt.plot(timeperiods, Y1, label='Y')
-            plt.axhline(y=ellbar, color='g')
-            plt.title('time series')
+            #plt.axhline(y=ellbar, color='g')
             plt.xlabel('time')
-            plt.legend(loc=9, ncol=(nx+ny))
+            # plt.legend(loc=9, ncol=(nx+ny))
             plt.show()    
     
         # Generate consumption, lambda, and gamma series
         r = alpha*X[0:T]**(alpha-1)*(A[0:T]*Y[0:T])**(1-alpha)
         w = (1-alpha)*X[0:T]**alpha*(A[0:T]*Y[0:T])**(-alpha)
-        c = (1-tau)*(w[0:T]*Y[0:T] + (r[0:T] - delta)*X[0:T]) + X[0:T] + tau*(w[0:T]*Y[0:T] + (r[0:T] - delta)*X[0:T]) - X[1:T+1]
+        c = (1-tau)*(w[0:T]*Y[0:T] + (r[0:T] - delta)*X[0:T]) + X[0:T] \
+            + tau*(w[0:T]*Y[0:T] + (r[0:T] - delta)*X[0:T]) - X[1:T+1]
         # T-by-1
         Lam = (c[0:T-1]**(-gam)*(1-tau)*w[0:T-1]) / (chi*Y[0:T-1]**theta)
         # (T-1)-by-1
-        Gam = (beta*c[1:T]**(-gam)*(1 + (1-tau)*(r[1:T] - delta))) / (c[0:T-1]**(-gam))
+        Gam = (beta*c[1:T]**(-gam)*(1 + (1-tau)*(r[1:T] - delta))) \
+            / (c[0:T-1]**(-gam))
         # (T-1)-by-1
     
         # update values for X and Y
@@ -268,7 +271,8 @@ def GSSA(params, kbar):
             coeffsnew = MVOLS(XY, x)
         
         dist = np.mean(np.abs(1-XY/XYold))
-        print('count ', count, 'distance', dist, 'distold', distold, 'damp', damp)
+        print('count ', count, 'distance', dist, 'distold', distold, \
+              'damp', damp)
         
         if dist < distold:
             damp = damp*1.05