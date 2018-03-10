25e527a68da28ffa47e719aebac015ab947de9d9
Kerk Phillips <kerk@pomosa.com>
2018-03-09 17:21:40 -0700

    Working on compute times.

--- Simple ILA/baseline2.py
+++ Simple ILA/baseline2.py
@@ -12,7 +12,7 @@ import timeit
 from ILArunmc import runmc
 from ILAmcanalysis import mcanalysis
 
-name = 'baseline_Anthonylaptop'
+name = 'baseline_Kerklaptop2'
 
 def generateLIN(k, z, args):
     from LinApp_Sim import LinApp_Sim
